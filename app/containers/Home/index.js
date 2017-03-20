02f78c5d37e6c0e98681a427d549a7495109850a
theClubhou.se <heythere@theclubhou.se>
2017-03-20 15:42:27 -0400

    added nav bar

---
+++ app/containers/Home/index.js
@@ -0,0 +1,32 @@
+/*
+ *
+ * Home
+ *
+ */
+
+import React from 'react';
+import Helmet from 'react-helmet';
+import {Link} from 'react-router';
+
+export default class Home extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
+
+          <header>
+            <nav>
+              <Link to="/"> Home </Link>
+              <Link to="/about"> About </Link>
+            </nav>
+          </header>
+
+          <main></main>
+
+          <footer></footer>
+
+
+      </div>
+    );
+  }
+}
